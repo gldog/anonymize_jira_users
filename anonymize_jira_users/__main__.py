88ab8d40dab43533dc4f22233f730e5e722044ee
Johannes Heger <gldog@gmx.de>
2021-04-05 15:30:05 +0200

    Fix: 'script_finished' was set after script generation
    This led to the warning "set_script_finished_date_and_execution_time detected an unexpected exit."

--- anonymize_jira_users/__main__.py
+++ anonymize_jira_users/__main__.py
@@ -6,17 +6,16 @@ from tools import Tools
 execution_logger = ExecutionLogger()
 execution_logger.logs['script_started'] = Tools.now_to_date_string()
 
-# Config creates and sets up the Logger.
+# Config also creates and sets up the Logger.
 config = Config()
-# config.execution_logger = execution_logger
-
-log = config.log
 
 cmd_executor = CmdExecutorFactory(config=config, execution_logger=execution_logger).new_instance()
 
 cmd_executor.check_cmd_parameters()
 cmd_executor.execute()
-cmd_executor.post_execute()
 
 execution_logger.logs['is_script_aborted'] = False
 execution_logger.logs['script_finished'] = Tools.now_to_date_string()
+
+# Write/print the report.
+cmd_executor.post_execute()
